dd9cf3652910d5d36ead53c6cf434084d9a32e87
Evan Shaw <evandanielshaw@gmail.com>
2019-06-12 11:45:28 +0900

    fix: radio button disappearing from payment select page

--- DisabledViews.php
+++ DisabledViews.php
@@ -81,79 +81,81 @@ class DisabledViews {
      */
     public function disablePaymentOption($html, $payments) {
         global $usces, $usces_entries;
-        $value = isset($usces_entries['order']['payment_name']) ? $usces_entries['order']['payment_name'] : '';
-        $payments = usces_get_system_option('usces_payment_method', 'sort');
-        $payments = apply_filters('usces_fiter_the_payment_method', $payments, $value);
+        if ($this->module->ready() === false || $this->module->isModuleActivated() === false) {
+            $value = isset($usces_entries['order']['payment_name']) ? $usces_entries['order']['payment_name'] : '';
+            $payments = usces_get_system_option('usces_payment_method', 'sort');
+            $payments = apply_filters('usces_fiter_the_payment_method', $payments, $value);
         
-        if (defined('WCEX_DLSELLER_VERSION') and version_compare(WCEX_DLSELLER_VERSION, '2.2-beta', '<=')) {
-            $cart = $usces->cart->get_cart();
-            $have_continue_charge = usces_have_continue_charge($cart);
-            $continue_payment_method = apply_filters('usces_filter_the_continue_payment_method', array( 'acting_remise_card', 'acting_paypal_ec' ));
-        }
-
-        $html = "<dl>\n";
-        $list = '';
-        $payment_ct = count($payments);
-        foreach ((array)$payments as $id => $payment) {
             if (defined('WCEX_DLSELLER_VERSION') and version_compare(WCEX_DLSELLER_VERSION, '2.2-beta', '<=')) {
-                if ($have_continue_charge) {
-                    if (!in_array($payment['settlement'], $continue_payment_method)) {
-                        $payment_ct--;
-                        continue;
-                    }
-                    if (isset($usces->options['acting_settings']['remise']['continuation']) && 'on' !== $usces->options['acting_settings']['remise']['continuation'] && 'acting_remise_card' == $payment['settlement']) {
-                        $payment_ct--;
-                        continue;
-                    } elseif (isset($usces->options['acting_settings']['paypal']['continuation']) && 'on' !== $usces->options['acting_settings']['paypal']['continuation'] && 'acting_paypal_ec' == $payment['settlement']) {
-                        $payment_ct--;
-                        continue;
-                    }
-                }
+                $cart = $usces->cart->get_cart();
+                $have_continue_charge = usces_have_continue_charge($cart);
+                $continue_payment_method = apply_filters('usces_filter_the_continue_payment_method', array( 'acting_remise_card', 'acting_paypal_ec' ));
             }
-            if ($payment['name'] != '' and $payment['use'] != 'deactivate') {
-                $module = trim($payment['module']);
-                if (!\WCUtils::is_blank($value)) {
-                    $checked = ($payment['name'] == $value) ? ' checked' : '';
-                } elseif (1 == $payment_ct) {
-                    $checked = ' checked';
-                } else {
-                    $checked = '';
-                }
-                $checked = apply_filters('usces_fiter_the_payment_method_checked', $checked, $payment, $value);
-                $explanation = apply_filters('usces_fiter_the_payment_method_explanation', $payment['explanation'], $payment, $value);
-                if ((empty($module) || !file_exists($usces->options['settlement_path'] . $module)) && $payment['settlement'] == 'acting') {
-                    $checked = '';
-                    $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' disabled onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name']).'</label> <b> (' . __('cannot use this payment method now.', 'usces') . ") </b></dt>\n";
-                } else {
-                    if ($payment['settlement'] === $this->module->getActingFlag()) {
-                        if ($this->module->ready() === false) {
-                            $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' disabled onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name']).'</label> <b> (' . sprintf(
-                                /* translators: %s: formatted module name. */
-                                __('Please validate your %1$s credentials to use %2$s', 'smodule'),
-                                $this->module->getAauth()->getProvider(),
-                                $this->module->getPaymentName()
-                            ) . ") </b></dt>\n";
-                        } elseif ($this->module->isModuleActivated() === false) {
-                            $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' disabled onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name']).'</label> <b> (' . sprintf(
-                                /* translators: %s: formatted module name. */
-                                __('%s is deactivated on the settlement module settings page.', 'smodule'),
-                                $this->module->getPaymentName()
-                            ) . ") </b></dt>\n";
+
+            $html = "<dl>\n";
+            $list = '';
+            $payment_ct = count($payments);
+            foreach ((array)$payments as $id => $payment) {
+                if (defined('WCEX_DLSELLER_VERSION') and version_compare(WCEX_DLSELLER_VERSION, '2.2-beta', '<=')) {
+                    if ($have_continue_charge) {
+                        if (!in_array($payment['settlement'], $continue_payment_method)) {
+                            $payment_ct--;
+                            continue;
                         }
-                    } else {
-                        $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name'])."</label></dt>\n";
+                        if (isset($usces->options['acting_settings']['remise']['continuation']) && 'on' !== $usces->options['acting_settings']['remise']['continuation'] && 'acting_remise_card' == $payment['settlement']) {
+                            $payment_ct--;
+                            continue;
+                        } elseif (isset($usces->options['acting_settings']['paypal']['continuation']) && 'on' !== $usces->options['acting_settings']['paypal']['continuation'] && 'acting_paypal_ec' == $payment['settlement']) {
+                            $payment_ct--;
+                            continue;
+                        }
                     }
                 }
-                $list .= "\t".'<dd class="payment_'.$id.'">'.$explanation.'</dd>'."\n";
+                if ($payment['name'] != '' and $payment['use'] != 'deactivate') {
+                    $module = trim($payment['module']);
+                    if (!\WCUtils::is_blank($value)) {
+                        $checked = ($payment['name'] == $value) ? ' checked' : '';
+                    } elseif (1 == $payment_ct) {
+                        $checked = ' checked';
+                    } else {
+                        $checked = '';
+                    }
+                    $checked = apply_filters('usces_fiter_the_payment_method_checked', $checked, $payment, $value);
+                    $explanation = apply_filters('usces_fiter_the_payment_method_explanation', $payment['explanation'], $payment, $value);
+                    if ((empty($module) || !file_exists($usces->options['settlement_path'] . $module)) && $payment['settlement'] == 'acting') {
+                        $checked = '';
+                        $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' disabled onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name']).'</label> <b> (' . __('cannot use this payment method now.', 'usces') . ") </b></dt>\n";
+                    } else {
+                        if ($payment['settlement'] === $this->module->getActingFlag()) {
+                            if ($this->module->ready() === false) {
+                                $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' disabled onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name']).'</label> <b> (' . sprintf(
+                                    /* translators: %s: formatted module name. */
+                                    __('Please validate your %1$s credentials to use %2$s', 'smodule'),
+                                    $this->module->getAauth()->getProvider(),
+                                    $this->module->getPaymentName()
+                                ) . ") </b></dt>\n";
+                            } elseif ($this->module->isModuleActivated() === false) {
+                                $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' disabled onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name']).'</label> <b> (' . sprintf(
+                                    /* translators: %s: formatted module name. */
+                                    __('%s is deactivated on the settlement module settings page.', 'smodule'),
+                                    $this->module->getPaymentName()
+                                ) . ") </b></dt>\n";
+                            }
+                        } else {
+                            $list .= "\t".'<dt class="payment_'.$id.'"><label for="payment_name_' . $id . '"><input name="offer[payment_name]" id="payment_name_' . $id . '" type="radio" value="'.esc_attr($payment['name']).'"' . $checked . ' onKeyDown="if (event.keyCode == 13) {return false;}" />'.esc_attr($payment['name'])."</label></dt>\n";
+                        }
+                    }
+                    $list .= "\t".'<dd class="payment_'.$id.'">'.$explanation.'</dd>'."\n";
+                }
+            }
+
+            $html .= $list . "</dl>\n";
+        
+            if (empty($list)) {
+                $html = __('Not yet ready for the payment method. Please refer to a manager.', 'usces')."\n";
             }
         }
-
-        $html .= $list . "</dl>\n";
         
-        if (empty($list)) {
-            $html = __('Not yet ready for the payment method. Please refer to a manager.', 'usces')."\n";
-        }
-
         return $html;
     }
 }
