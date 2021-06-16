ea757085a3ba9a4f0064cf336ebc1b1a55b9b15f
Evan Shaw <evandanielshaw@gmail.com>
2021-06-16 18:07:33 +0900

    fix: fix HooksAutoloader which was erroneously registering unimplemented hooks

--- src/HooksAutoloader.php
+++ src/HooksAutoloader.php
@@ -11,13 +11,6 @@ use ReflectionException;
 trait HooksAutoloader
 {
     /**
-     * Reflection of subclass instance
-     *
-     * @var ReflectionClass|null
-     */
-    private $child = null;
-
-    /**
      * Loops through `$hookMetaMap` and invokes `dynamicallyRegisterHook` method
      *
      * @author Evan D Shaw <evandanielshaw@gmail.com>
@@ -25,31 +18,38 @@ trait HooksAutoloader
      * @return void
      */
     public function dynamicallyRegisterHooks(array $hookMetaMap) {
-        $this->child = new ReflectionClass($this);
+        $currentInstance = new ReflectionClass($this);
         foreach ($hookMetaMap as $hookMeta) {
-            $this->dynamicallyRegisterHook($hookMeta);
+            $this->dynamicallyRegisterHook($hookMeta, $currentInstance);
         }
     }
 
     /**
-     * Finds all parent methods invoked by an action/filter hook that are redeclared in child class
+     * Finds all parent methods invoked by an action/filter hook that are redeclared in a child class
      *
      * If a redeclaration is found, the corresponding action/filter hook will be added.
      *
      * @author Evan D Shaw <evandanielshaw@gmail.com>
-     * @param HookMeta $hookMeta
+     * @param HookMeta        $hookMeta
+     * @param ReflectionClass $currentInstance
      * @return void
      */
-    public function dynamicallyRegisterHook(HookMeta $hookMeta) {
-        if ($this->child === null) {
-            $this->child = new ReflectionClass($this);
-        }
+    public function dynamicallyRegisterHook(HookMeta $hookMeta, $currentInstance) {
         foreach ($hookMeta->getMethodsInvokedByHook() as $methodName) {
             try {
-                $this->child->getMethod($methodName); // will throw exception if method doesn't exist
-                if (is_callable($hookMeta->getHook())) {
-                    $hookMeta->callHook();
-                    break;
+                // will throw exception if method doesn't exist
+                $method = $currentInstance->getMethod($methodName);
+                $parent = $currentInstance->getParentClass();
+                // if current instance is not an extended class, optional hooks wont do anything anyways...
+                if ($parent !== false) {
+                    // only register hook if declared in a child class
+                    if ($method->class !== $parent->name) {
+                        if (is_callable($hookMeta->getHook())) {
+                            // registers the action or filter
+                            $hookMeta->callHook();
+                            break;
+                        }
+                    }
                 }
             } catch (ReflectionException $e) {
                 // method doesn't exist...
