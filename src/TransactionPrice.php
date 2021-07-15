263f606b8cee4671f5d4ddf86d69495b92f555f5
Evan Shaw <evandanielshaw@gmail.com>
2021-07-15 20:29:05 +0900

    feat: add static variants for TransactionPrice methods

--- src/TransactionPrice.php
+++ src/TransactionPrice.php
@@ -62,6 +62,17 @@ class TransactionPrice implements JsonSerializable, SerializeTargetSettable
     /**
      * Returns the formatted price
      *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @see getFormattedAmountWithSymbolByCurrencyCode()
+     * @return string
+     */
+    public function getFormattedAmountWithSymbol() {
+        return self::getFormattedAmountWithSymbolByCurrencyCode($this->currencyCode, $this->amount);
+    }
+
+    /**
+     * Returns the formatted price
+     *
      * The amount is formatted with a decimal point and thousands separator
      * depending on whether the currency code requires it or not.
      *
@@ -76,18 +87,31 @@ class TransactionPrice implements JsonSerializable, SerializeTargetSettable
      * @author Evan D Shaw <evandanielshaw@gmail.com>
      * @global array $usces_settings
      * @global \usc_e_shop $usces
+     * @param string    $currencyCode
+     * @param int|float $amount
      * @return string
      */
-    public function getFormattedAmountWithSymbol() {
+    public static function getFormattedAmountWithSymbolByCurrencyCode($currencyCode, $amount) {
         global $usces_settings, $usces;
 
         $cc = $usces->options['system']['currency'];
         $code = isset($usces_settings['currency'][$cc][0]) ? $usces_settings['currency'][$cc][0] : null;
-        if ($code !== $this->currencyCode) {
-            return $this->currencyCode . $this->getFormattedAmount();
+        if ($code !== $currencyCode) {
+            return $currencyCode . self::getFormattedAmountByCurrencyCode($currencyCode, $amount);
         }
 
-        return usces_crform($this->amount, false, true, 'return', true);
+        return usces_crform($amount, false, true, 'return', true);
+    }
+
+    /**
+     * Returns the formatted amount based on the currency code
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @see getFormattedAmountByCurrencyCode()
+     * @return string
+     */
+    public function getFormattedAmount() {
+        return self::getFormattedAmountByCurrencyCode($this->currencyCode, $this->amount);
     }
 
     /**
@@ -97,11 +121,24 @@ class TransactionPrice implements JsonSerializable, SerializeTargetSettable
      * depending on whether the currency code requires it or not.
      *
      * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @param string    $currencyCode
+     * @param int|float $amount
      * @return string
      */
-    public function getFormattedAmount() {
-        list($decimal, $point, $seperator) = Currency::ISO_4217_DISPLAY_META_MAP[$this->currencyCode];
-        return number_format((double)$this->amount, $decimal, $point, $seperator);
+    public static function getFormattedAmountByCurrencyCode($currencyCode, $amount) {
+        list($decimal, $point, $seperator) = Currency::ISO_4217_DISPLAY_META_MAP[$currencyCode];
+        return number_format((double)$amount, $decimal, $point, $seperator);
+    }
+
+    /**
+     * Returns the raw amount
+     *
+     * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @see getRawAmountByCurrencyCode()
+     * @return string
+     */
+    public function getRawAmount() {
+        return self::getRawAmountByCurrencyCode($this->currencyCode, $this->amount);
     }
 
     /**
@@ -110,16 +147,18 @@ class TransactionPrice implements JsonSerializable, SerializeTargetSettable
      * Depending on the currency code, the raw amount may or may not contain
      * decimal digits but will not contain a thousands separator.
      *
-     * If the amount passed to the constructor was not in the proper format for the
+     * If the amount passed to this method is not in the proper format for the
      * currency code, this method will append/strip decimal digits accordingly and
      * return the result.
      *
      * @author Evan D Shaw <evandanielshaw@gmail.com>
+     * @param string    $currencyCode
+     * @param int|float $amount
      * @return string
      */
-    public function getRawAmount() {
-        list($decimal, $point) = Currency::ISO_4217_DISPLAY_META_MAP[$this->currencyCode];
-        return number_format((double)$this->amount, $decimal, $point, '');
+    public static function getRawAmountByCurrencyCode($currencyCode, $amount) {
+        list($decimal, $point) = Currency::ISO_4217_DISPLAY_META_MAP[$currencyCode];
+        return number_format((double)$amount, $decimal, $point, '');
     }
 
     /**
